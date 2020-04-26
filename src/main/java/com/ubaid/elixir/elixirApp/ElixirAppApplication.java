e6c4222e85a51aecddbbbdca32a3ef4a4a45c4f8
UbaidurRehman1 <ubaidshan007@gmail.com>
2020-04-26 17:15:43 +0500

    init

---
+++ src/main/java/com/ubaid/elixir/elixirApp/ElixirAppApplication.java
@@ -0,0 +1,13 @@
+package com.ubaid.elixir.elixirApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElixirAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElixirAppApplication.class, args);
+	}
+
+}
